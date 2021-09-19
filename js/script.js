dd657de45057adaa2ec075f47aae3f186f69ebee
Iwona007 <iwonka.adamkiewicz@gmail.com>
2021-09-19 17:38:21 +0200

    change js metodes

--- js/script.js
+++ js/script.js
@@ -2,41 +2,29 @@ let plnElement = document.querySelector(".js-pln");
 let usdElement = document.querySelector(".usd");
 let audElement = document.querySelector(".aud");
 let formElement = document.querySelector(".js-form");
-
 let calculatedElement = document.querySelector(".js-calculate");
-let chosenCurrency = document.querySelector(".js-chosenCurrency");
-
 let changeCurrency = document.querySelector(".changeCurrency");
+
+let usd = 3.92;
+let aud = 2.85;
+let result = 0.00;
+let pln = parseFloat(plnElement.value);
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let pln = parseFloat(plnElement.value);
-    let chosen = parseFloat(chosenCurrency.value);
-    let result;
-    result = pln * chosen;
-
+    switch (changeCurrency.value) {
+        case "usd":
+            console.log("Wybrano usd")
+            result = plnElement.value * usd;
+            break;
+        case "aud":
+            console.log("Wybrano aud")
+            result = plnElement.value * aud;
+            break;
+        default:
+            result;
+    }
     calculatedElement.innerText = result.toFixed(2);
     console.log(`Przeliczono, wynik to: ${result}`)
 });
-
-changeCurrency.addEventListener("input", () => {
-
-    switch (changeCurrency.value) {
-        case "usd":
-            chosenCurrency.value = 3.92;
-            console.log("Wybrano usd")
-        case "aud":
-            chosenCurrency.value = 2.85;
-            console.log("Wybrano aud")
-    }
-});
-
-// changeCurrency.addEventListener("click", e => {
-//     if (changeCurrency.classList.contains("usd")) {
-//         console.log("Wybrano walutę: usd")
-//     } else {
-//         console.log("Wybrano walutę: aud")
-//     }
-
-// })
