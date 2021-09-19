5d321f97ea485caddcf191406b31e0f7f98140c6
Iwona007 <iwonka.adamkiewicz@gmail.com>
2021-09-19 18:47:15 +0200

    clean js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@ let plnElement = document.querySelector(".js-pln");
 let usdElement = document.querySelector(".usd");
 let audElement = document.querySelector(".aud");
 let formElement = document.querySelector(".js-form");
-
 let calculatedElement = document.querySelector(".js-calculate");
 let chosenCurrency = document.querySelector(".js-chosenCurrency");
-
 let changeCurrency = document.querySelector(".changeCurrency");
+
+changeCurrency.addEventListener("input", () => {
+
+    switch (changeCurrency.value) {
+        case "usd":
+            chosenCurrency.value = 3.92;
+        case "aud":
+            chosenCurrency.value = 2.85;
+    }
+});
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -17,26 +25,4 @@ formElement.addEventListener("submit", (event) => {
     result = pln * chosen;
 
     calculatedElement.innerText = result.toFixed(2);
-    console.log(`Przeliczono, wynik to: ${result}`)
 });
-
-changeCurrency.addEventListener("input", () => {
-
-    switch (changeCurrency.value) {
-        case "usd":
-            chosenCurrency.value = 3.92;
-            console.log("Wybrano usd")
-        case "aud":
-            chosenCurrency.value = 2.85;
-            console.log("Wybrano aud")
-    }
-});
-
-// changeCurrency.addEventListener("click", e => {
-//     if (changeCurrency.classList.contains("usd")) {
-//         console.log("Wybrano walutę: usd")
-//     } else {
-//         console.log("Wybrano walutę: aud")
-//     }
-
-// })
