d6958c68aaad3fe789e4d3e3be23dd4bca959e91
Iwona007 <iwonka.adamkiewicz@gmail.com>
2022-04-07 17:04:49 +0200

    refactoring in js file and html

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
-let plnElement = document.querySelector(".js-pln");
-// let usdElement = document.querySelector(".usd");
-// let audElement = document.querySelector(".aud");
-let formElement = document.querySelector(".js-form");
-let calculatedElement = document.querySelector(".js-calculate");
-let chosenCurrency = document.querySelector(".js-chosenCurrency");
-let chosen = parseFloat(chosenCurrency.value);
+{
+    const calculateResult = (plnAmount, chosenCurrency, rateCurrency) => {
+        switch (chosenCurrency) {
+            case chosenCurrency:
+                return plnAmount / rateCurrency;
+            case chosenCurrency:
+                return plnAmount / rateCurrency;
+        }
+    }
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const onResultText = (plnAmount, result, chosenCurrency) => {
+        const calculatedElement = document.querySelector(".js-calculate");
+        calculatedElement.innerHTML = `${plnAmount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${chosenCurrency}</strong>`;
+    };
 
-    let pln = parseFloat(plnElement.value);
-    let chosen = parseFloat(chosenCurrency.value);
-    let result;
-    result = pln * chosen;
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const plnAmountElement = document.querySelector(".js-plnAmount");
+        const chosenCurrencyElement = document.querySelector(".js-chosenCurrency");
+        const chosenRateCurrencyElement = document.querySelector(".js-rateCurrency")
 
-    calculatedElement.innerText = result.toFixed(2);
-});
+        const plnAmount = +plnAmountElement.value;
+        const chosenCurrency = chosenCurrencyElement.value;
+        const rateCurrency = +chosenRateCurrencyElement.value;
+
+        const result = calculateResult(plnAmount, chosenCurrency, rateCurrency);
+
+        onResultText(plnAmount, result, chosenCurrency);
+    }
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+    }
+
+    init();
+}
+
